556c2f9c25502dbcca68064442bd8917f549c051
Vuze <vuze.mhf2resg@gmail.com>
2016-03-25 21:39:53 +0100

    Initial push from local

---
+++ QuickSouls/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace QuickSouls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelDir = new System.Windows.Forms.Label();
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.buttonSetDir = new System.Windows.Forms.Button();
+            this.labelSeperator1 = new System.Windows.Forms.Label();
+            this.labelQSKey = new System.Windows.Forms.Label();
+            this.labelQLKey = new System.Windows.Forms.Label();
+            this.buttonSetKey = new System.Windows.Forms.Button();
+            this.labelSeperator2 = new System.Windows.Forms.Label();
+            this.buttonLaunch = new System.Windows.Forms.Button();
+            this.checkBoxSound = new System.Windows.Forms.CheckBox();
+            this.labelCredits = new System.Windows.Forms.Label();
+            this.hotkeyControlQL = new exscape.HotkeyControl();
+            this.hotkeyControlQS = new exscape.HotkeyControl();
+            this.SuspendLayout();
+            // 
+            // labelDir
+            // 
+            this.labelDir.AutoSize = true;
+            this.labelDir.Location = new System.Drawing.Point(12, 13);
+            this.labelDir.Name = "labelDir";
+            this.labelDir.Size = new System.Drawing.Size(148, 13);
+            this.labelDir.TabIndex = 0;
+            this.labelDir.Text = "Dark Souls Save Data Folder:";
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Location = new System.Drawing.Point(162, 10);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(275, 20);
+            this.textBoxDir.TabIndex = 1;
+            // 
+            // buttonSetDir
+            // 
+            this.buttonSetDir.Location = new System.Drawing.Point(443, 8);
+            this.buttonSetDir.Name = "buttonSetDir";
+            this.buttonSetDir.Size = new System.Drawing.Size(78, 23);
+            this.buttonSetDir.TabIndex = 2;
+            this.buttonSetDir.Text = "Set";
+            this.buttonSetDir.UseVisualStyleBackColor = true;
+            this.buttonSetDir.Click += new System.EventHandler(this.buttonSetDir_Click);
+            // 
+            // labelSeperator1
+            // 
+            this.labelSeperator1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelSeperator1.Location = new System.Drawing.Point(13, 38);
+            this.labelSeperator1.Name = "labelSeperator1";
+            this.labelSeperator1.Size = new System.Drawing.Size(509, 2);
+            this.labelSeperator1.TabIndex = 3;
+            // 
+            // labelQSKey
+            // 
+            this.labelQSKey.AutoSize = true;
+            this.labelQSKey.Location = new System.Drawing.Point(13, 50);
+            this.labelQSKey.Name = "labelQSKey";
+            this.labelQSKey.Size = new System.Drawing.Size(98, 13);
+            this.labelQSKey.TabIndex = 4;
+            this.labelQSKey.Text = "Quicksave Hotkey:";
+            // 
+            // labelQLKey
+            // 
+            this.labelQLKey.AutoSize = true;
+            this.labelQLKey.Location = new System.Drawing.Point(214, 50);
+            this.labelQLKey.Name = "labelQLKey";
+            this.labelQLKey.Size = new System.Drawing.Size(95, 13);
+            this.labelQLKey.TabIndex = 6;
+            this.labelQLKey.Text = "Quickload Hotkey:";
+            // 
+            // buttonSetKey
+            // 
+            this.buttonSetKey.Location = new System.Drawing.Point(443, 45);
+            this.buttonSetKey.Name = "buttonSetKey";
+            this.buttonSetKey.Size = new System.Drawing.Size(78, 23);
+            this.buttonSetKey.TabIndex = 8;
+            this.buttonSetKey.Text = "Set";
+            this.buttonSetKey.UseVisualStyleBackColor = true;
+            this.buttonSetKey.Click += new System.EventHandler(this.buttonSetKey_Click);
+            // 
+            // labelSeperator2
+            // 
+            this.labelSeperator2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelSeperator2.Location = new System.Drawing.Point(12, 74);
+            this.labelSeperator2.Name = "labelSeperator2";
+            this.labelSeperator2.Size = new System.Drawing.Size(509, 2);
+            this.labelSeperator2.TabIndex = 9;
+            // 
+            // buttonLaunch
+            // 
+            this.buttonLaunch.Location = new System.Drawing.Point(12, 85);
+            this.buttonLaunch.Name = "buttonLaunch";
+            this.buttonLaunch.Size = new System.Drawing.Size(195, 35);
+            this.buttonLaunch.TabIndex = 10;
+            this.buttonLaunch.Text = "Register Hotkeys";
+            this.buttonLaunch.UseVisualStyleBackColor = true;
+            this.buttonLaunch.Click += new System.EventHandler(this.buttonLaunch_Click);
+            // 
+            // checkBoxSound
+            // 
+            this.checkBoxSound.AutoSize = true;
+            this.checkBoxSound.Checked = true;
+            this.checkBoxSound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSound.Location = new System.Drawing.Point(213, 95);
+            this.checkBoxSound.Name = "checkBoxSound";
+            this.checkBoxSound.Size = new System.Drawing.Size(161, 17);
+            this.checkBoxSound.TabIndex = 11;
+            this.checkBoxSound.Text = "Play Sound after Save/Load";
+            this.checkBoxSound.UseVisualStyleBackColor = true;
+            this.checkBoxSound.CheckedChanged += new System.EventHandler(this.checkBoxSound_CheckedChanged);
+            // 
+            // labelCredits
+            // 
+            this.labelCredits.AutoSize = true;
+            this.labelCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCredits.Location = new System.Drawing.Point(365, 140);
+            this.labelCredits.Name = "labelCredits";
+            this.labelCredits.Size = new System.Drawing.Size(157, 13);
+            this.labelCredits.TabIndex = 12;
+            this.labelCredits.Text = "QuickSouls v1 by MHVuze";
+            // 
+            // hotkeyControlQL
+            // 
+            this.hotkeyControlQL.Hotkey = System.Windows.Forms.Keys.F8;
+            this.hotkeyControlQL.HotkeyModifiers = System.Windows.Forms.Keys.None;
+            this.hotkeyControlQL.Location = new System.Drawing.Point(315, 47);
+            this.hotkeyControlQL.Name = "hotkeyControlQL";
+            this.hotkeyControlQL.Size = new System.Drawing.Size(90, 20);
+            this.hotkeyControlQL.TabIndex = 7;
+            this.hotkeyControlQL.Text = "F8";
+            // 
+            // hotkeyControlQS
+            // 
+            this.hotkeyControlQS.Hotkey = System.Windows.Forms.Keys.F7;
+            this.hotkeyControlQS.HotkeyModifiers = System.Windows.Forms.Keys.None;
+            this.hotkeyControlQS.Location = new System.Drawing.Point(117, 47);
+            this.hotkeyControlQS.Name = "hotkeyControlQS";
+            this.hotkeyControlQS.Size = new System.Drawing.Size(90, 20);
+            this.hotkeyControlQS.TabIndex = 5;
+            this.hotkeyControlQS.Text = "F7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 162);
+            this.Controls.Add(this.labelCredits);
+            this.Controls.Add(this.checkBoxSound);
+            this.Controls.Add(this.buttonLaunch);
+            this.Controls.Add(this.hotkeyControlQL);
+            this.Controls.Add(this.hotkeyControlQS);
+            this.Controls.Add(this.labelSeperator2);
+            this.Controls.Add(this.buttonSetKey);
+            this.Controls.Add(this.labelQLKey);
+            this.Controls.Add(this.labelQSKey);
+            this.Controls.Add(this.labelSeperator1);
+            this.Controls.Add(this.buttonSetDir);
+            this.Controls.Add(this.textBoxDir);
+            this.Controls.Add(this.labelDir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "QuickSouls";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDir;
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.Button buttonSetDir;
+        private System.Windows.Forms.Label labelSeperator1;
+        private System.Windows.Forms.Label labelQSKey;
+        private System.Windows.Forms.Label labelQLKey;
+        private System.Windows.Forms.Button buttonSetKey;
+        private System.Windows.Forms.Label labelSeperator2;
+        private exscape.HotkeyControl hotkeyControlQS;
+        private exscape.HotkeyControl hotkeyControlQL;
+        private System.Windows.Forms.Button buttonLaunch;
+        private System.Windows.Forms.CheckBox checkBoxSound;
+        private System.Windows.Forms.Label labelCredits;
+    }
+}
+
---
+++ QuickSouls/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Media;
+
+namespace QuickSouls
+{
+    public partial class Form1 : Form
+    {
+        // Set up variables
+        string SaveDir = "";
+        int QSKey = 0;
+        int QSKeyMod = 0;
+        int QLKey = 0;
+        int QLKeyMod = 0;
+        bool SoundFlag = true;
+
+        // Enable global hotkey functionality
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Sound Play
+        private void playSound()
+        {
+            SoundPlayer Sound = new SoundPlayer(@"sound.wav");
+            Sound.Play();
+        }
+
+        // Form
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Check for config file
+            if (!File.Exists(@"QuickSouls.ini"))
+            {
+                string[] settings = { 
+                                        Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents\\NBGI\\DarkSouls\\"),  // Default save data path
+                                        ((int)Keys.F7).ToString(),                                                                              // Default quick save hotkey
+                                        ((int)Keys.None).ToString(),                                                                            // Default quick save hotkey modifier
+                                        ((int)Keys.F8).ToString(),                                                                              // Default quick load hotkey
+                                        ((int)Keys.None).ToString(),                                                                            // Default quick load hotkey modifier
+                                        1.ToString()                                                                                            // Default sound property                     
+                                    };
+
+                using (StreamWriter defaultConfig = new StreamWriter(@"QuickSouls.ini"))
+                {
+                    foreach (string line in settings)
+                        defaultConfig.WriteLine(line);
+                }
+
+                SaveDir = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents\\NBGI\\DarkSouls\\");
+                QSKey = (int)Keys.F7;
+                QSKeyMod = (int)Keys.None;
+                QLKey = (int)Keys.F8;
+                QLKeyMod = (int)Keys.None;
+                SoundFlag = true;
+            }
+            else
+            {
+                StreamReader ConfigReader = new StreamReader(@"QuickSouls.ini");
+                SaveDir = ConfigReader.ReadLine();
+                QSKey = int.Parse(ConfigReader.ReadLine());
+                QSKeyMod = int.Parse(ConfigReader.ReadLine());
+                QLKey = int.Parse(ConfigReader.ReadLine());
+                QLKeyMod = int.Parse(ConfigReader.ReadLine());
+                SoundFlag = int.Parse(ConfigReader.ReadLine()) == 1 ? true : false;
+                ConfigReader.Close();
+            }
+
+            // Fill boxes
+            textBoxDir.Text = SaveDir;
+            hotkeyControlQS.Hotkey = (Keys)QSKey;
+            hotkeyControlQS.HotkeyModifiers = (Keys)QSKeyMod;
+            hotkeyControlQL.Hotkey = (Keys)QLKey;
+            hotkeyControlQL.HotkeyModifiers = (Keys)QLKeyMod;
+            checkBoxSound.Checked = SoundFlag == true ? true : false;
+        }
+
+        // 'Set save directory' button
+        private void buttonSetDir_Click(object sender, EventArgs e)
+        {
+            SaveDir = textBoxDir.Text;
+            var lines = File.ReadAllLines(@"QuickSouls.ini");
+            lines[0] = SaveDir;
+            File.WriteAllLines(@"QuickSouls.ini", lines);
+
+            MessageBox.Show(
+                "Save directory has been set to:\n" + SaveDir,
+                "Information",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // 'Set hotkeys' button
+        private void buttonSetKey_Click(object sender, EventArgs e)
+        {
+            var lines = File.ReadAllLines(@"QuickSouls.ini");
+            lines[1] = ((int)hotkeyControlQS.Hotkey).ToString();
+            lines[2] = ((int)hotkeyControlQS.HotkeyModifiers).ToString();
+            lines[3] = ((int)hotkeyControlQL.Hotkey).ToString();
+            lines[4] = ((int)hotkeyControlQL.HotkeyModifiers).ToString();
+            File.WriteAllLines(@"QuickSouls.ini", lines);
+
+            MessageBox.Show(
+                "Hotkeys have been set.",
+                "Information",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // Sound enable/disable
+        private void checkBoxSound_CheckedChanged(object sender, EventArgs e)
+        {
+            var lines = File.ReadAllLines(@"QuickSouls.ini");
+            lines[5] = checkBoxSound.Checked == true ? "1" : "0";
+            File.WriteAllLines(@"QuickSouls.ini", lines);
+
+            SoundFlag = checkBoxSound.Checked == true ? true : false;
+        }
+
+        // Register Hotkeys
+        private void buttonLaunch_Click(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, 1, (int)hotkeyControlQS.HotkeyModifiers, (int)hotkeyControlQS.Hotkey);
+            RegisterHotKey(this.Handle, 2, (int)hotkeyControlQL.HotkeyModifiers, (int)hotkeyControlQL.Hotkey);
+
+            MessageBox.Show(
+                "Hotkeys have been registered.",
+                "Information",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // Execute stuff
+        protected override void WndProc(ref Message m)
+        {
+            // Quick Save
+            if (m.Msg == 0x0312 && (int)m.WParam == 1)
+            {
+                if (File.Exists(SaveDir + "DRAKS0005.sl2"))
+                {
+                    File.Copy(SaveDir + "DRAKS0005.sl2", @"quicksave.sl2", true);
+                    if (SoundFlag == true)
+                        playSound();
+                }
+            }
+
+            // Quick Load
+            if (m.Msg == 0x0312 && (int)m.WParam == 2)
+            {
+                if (File.Exists(@"quicksave.sl2"))
+                {
+                    try { 
+                        File.Copy(@"quicksave.sl2", SaveDir + "DRAKS0005.sl2", true);
+                        if (SoundFlag == true)
+                            playSound();
+                        }
+                    catch { MessageBox.Show("Could not complete quick load.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        // Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Form Close
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 1);
+            UnregisterHotKey(this.Handle, 2);
+        }
+    }
+}
